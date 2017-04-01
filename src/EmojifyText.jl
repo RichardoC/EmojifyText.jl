2d7139a09ae33474886e6f5c9b2708894975e574
Richard Finlay Tweed <richardftweed@gmail.com>
2017-04-01 16:09:19 +0100

    Added unconvert functionality to EmojifyText and stopped it printing so much

--- src/EmojifyText.jl
+++ src/EmojifyText.jl
@@ -1,41 +1,54 @@
 module EmojifyText
 include("EmojiDict.jl")
-# include(EmojiDict)
 
 # package code goes here
 
 conversion_dict = conversion_dict
-# conversion_dict["\n"] = "\n"
 
 function convert_line(line::String)
-  newline = ""
+  newline = "";
   for char in line
-    # println(char)
-    if char == '\n'
-      continue
-    end
-    try
-      newchar = conversion_dict[string(char)]
-      # println(newchar)
-      newline = newline * newchar
-    catch err
-      showerror(STDOUT, err, backtrace());println()
-    end
+    newchar = get(conversion_dict, string(char), "🚓");
+    newline = newline * newchar;
   end
-  # println(newline)
-  return newline
+  return newline;
+end
+
+function unconvert_line(line::String)
+  newline = "";
+  for char in line
+    newchar = get(dict_conversion, string(char), "🚓");
+    newline = newline * newchar;
+  end
+  return newline;
 end
 
 function convert_file(input_file_name::String, output_file_name::String)
   f_in = open(input_file_name,"r")
   f_out = open(output_file_name, "w")
+  #Deleting the file if it exists
   print(f_out,"")
   close(f_out)
   f_out = open(output_file_name, "a")
   for line in eachline(f_in)
-    newline = convert_line(line)
-    println(f_out,newline)
-    println(newline)
+    newline = convert_line(line);
+    print(f_out,newline);
+  end
+  close(f_in)
+  close(f_out)
+  print_file(output_file_name)
+end
+
+function unconvert_file(input_file_name::String, output_file_name::String)
+  f_in = open(input_file_name,"r")
+  f_out = open(output_file_name, "w")
+  #Deleting the file if it exists
+  print(f_out,"")
+  close(f_out)
+  f_out = open(output_file_name, "a")
+  for line in eachline(f_in)
+    newline = unconvert_line(line);
+    print(f_out,newline);
   end
   close(f_in)
   close(f_out)
@@ -45,6 +58,7 @@ end
 function print_file(file_name::String)
   f_n = open(file_name,"r")
   println("Printing file "*file_name)
+  println("-----------------------------------------")
   for line in eachline(f_n)
     print(line)
   end
