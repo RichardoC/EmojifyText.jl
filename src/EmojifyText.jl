b81f4f67e8c0fe7640e7e728d0db355d5b199f67
Richard Finlay Tweed <richardftweed@gmail.com>
2017-04-01 14:34:24 +0100

    Added initial functionality to EmojifyText

--- src/EmojifyText.jl
+++ src/EmojifyText.jl
@@ -1,5 +1,54 @@
 module EmojifyText
-
+include("EmojiDict.jl")
 # package code goes here
+
+conversion_dict = conversion_dict
+# conversion_dict["\n"] = "\n"
+
+function convert_line(line::String)
+  newline = ""
+  for char in line
+    println(char)
+    if char == '\n'
+      continue
+    end
+    try
+      newchar = conversion_dict[string(char)]
+      println(newchar)
+      newline = newline * newchar
+      println(newline)
+    catch err
+      showerror(STDOUT, err, backtrace());println()
+    end
+  end
+  return newline
+end
+
+function convert_file(input_file_name::String, output_file_name::String)
+  f_in = open(input_file_name,"r")
+  f_out = open(output_file_name, "w")
+  print(f_out,"")
+  close(f_out)
+  f_out = open(output_file_name, "a")
+  for line in eachline(f_in)
+    newline = convert_line(line)
+    println(f_out,newline)
+    println(newline)
+  end
+  close(f_in)
+  close(f_out)
+  print_file(output_file_name)
+end
+
+function print_file(file_name::String)
+  f_n = open(file_name,"r")
+  println("Printing file "*file_name)
+  for line in eachline(f_n)
+    print(line)
+  end
+  close(f_n)
+end
+
+
 
 end # module
